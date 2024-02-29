41bd971508371af7cf3e5ac61d54e6d353aa85e4
yagouam <yago.bernabe@uam.es>
2024-02-29 09:05:01 +0100

    Update settings to include filters

--- uamx_social_auth/apps.py
+++ uamx_social_auth/apps.py
@@ -3,7 +3,7 @@ uamx_social_auth Django application initialization.
 """
 
 from django.apps import AppConfig
-
+from edx_django_utils.plugins.constants import PluginSettings
 
 class UamxSocialAuthConfig(AppConfig):
     """
@@ -11,3 +11,29 @@ class UamxSocialAuthConfig(AppConfig):
     """
 
     name = 'uamx_social_auth'
+
+    # Class attribute that configures and enables this app as a Plugin App.
+    plugin_app = {
+
+        # Configuration setting for Plugin Settings for this app.
+        PluginSettings.CONFIG: {
+
+            # Configure the Plugin Settings for each Project Type, as needed. The full list of setting types for edx-platform is
+            # here:
+            # https://github.com/openedx/edx-platform/blob/2dc79bcab42dafed2c122eb808cdd5604327c890/openedx/core/djangoapps/plugins/constants.py#L25 .
+            # Other IDAs may use different values.
+            'lms.djangoapp': {
+
+                # Configure each settings, as needed.
+                'production': {
+
+                    # The python path (relative to this app) to the settings module for the relevant Project Type and Settings Type.
+                    # Optional; Defaults to 'settings'.
+                    PluginSettings.RELATIVE_PATH: 'settings.production',
+                },
+                'common': {
+                    PluginSettings.RELATIVE_PATH: 'settings.common',
+                }
+            }
+        }
+    }
---
+++ uamx_social_auth/settings/production.py
@@ -0,0 +1,28 @@
+# Add Filter configuration to show Terms of Service modal on every user login
+# based on https://docs.openedx.org/projects/openedx-filters/en/latest/how-tos/using-filters.html
+
+# lint-amnesty, pylint: disable=missing-function-docstring, missing-module-docstring
+def plugin_settings(settings):
+
+    print('************************')
+    print('************************')
+    print('plugin_settings PRODUCTION')
+    print('************************')
+    print('************************')
+
+    # Add plugin configuration based on 
+    # https://github.com/eduNEXT/openedx-filters-samples/blob/c22cf063ccc82b862310b8dae45731ecb3abdd73/openedx_filters_samples/samples/pipeline.py#L273
+    settings.OPEN_EDX_FILTERS_CONFIG = {
+            "org.openedx.learning.student.registration.requested.v1": {
+                "fail_silently": False,
+                "pipeline": [
+                    "uamx_social_auth.pipeline.StopUAMDomainRegister"
+                ]
+            },
+            "org.openedx.learning.student.login.requested.v1": {
+                "fail_silently": False,
+                "pipeline": [
+                    "uamx_social_auth.pipeline.StopUAMDomainLogin"
+                ]
+            }
+        }
