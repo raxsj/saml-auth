030e373f6484fbbeafbe597153408b9ef2523013
Angel Lareo <angel.lareo@gmail.com>
2024-02-29 09:50:40 +0100

    Remove prints

--- uamx_social_auth/settings/production.py
+++ uamx_social_auth/settings/production.py
@@ -4,12 +4,6 @@
 # lint-amnesty, pylint: disable=missing-function-docstring, missing-module-docstring
 def plugin_settings(settings):
 
-    print('************************')
-    print('************************')
-    print('plugin_settings PRODUCTION')
-    print('************************')
-    print('************************')
-
     # Add plugin configuration based on 
     # https://github.com/eduNEXT/openedx-filters-samples/blob/c22cf063ccc82b862310b8dae45731ecb3abdd73/openedx_filters_samples/samples/pipeline.py#L273
     settings.OPEN_EDX_FILTERS_CONFIG = {
