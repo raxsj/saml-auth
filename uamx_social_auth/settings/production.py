52abc6c80c5d8ceb525dd3b95c208f243c073f3b
Angel Lareo <angel.lareo@gmail.com>
2024-05-23 19:49:49 +0200

    comentados filtros de registro

--- uamx_social_auth/settings/production.py
+++ uamx_social_auth/settings/production.py
@@ -7,12 +7,12 @@ def plugin_settings(settings):
     # Add plugin configuration based on 
     # https://github.com/eduNEXT/openedx-filters-samples/blob/c22cf063ccc82b862310b8dae45731ecb3abdd73/openedx_filters_samples/samples/pipeline.py#L273
     settings.OPEN_EDX_FILTERS_CONFIG = {
-            "org.openedx.learning.student.registration.requested.v1": {
-                "fail_silently": False,
-                "pipeline": [
-                    "uamx_social_auth.pipeline.StopUAMDomainRegister"
-                ]
-            },
+            #"org.openedx.learning.student.registration.requested.v1": {
+            #    "fail_silently": False,
+            #    "pipeline": [
+            #        "uamx_social_auth.pipeline.StopUAMDomainRegister"
+            #    ]
+            #},
             "org.openedx.learning.student.login.requested.v1": {
                 "fail_silently": False,
                 "pipeline": [
