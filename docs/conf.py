a62cf04688e823a139e85b75cc29c213e70841c9
yagouam <yago.bernabe@uam.es>
2025-03-17 12:36:59 +0100

    Change version to 0.1.2 as pip do not understand .test ending

--- docs/conf.py
+++ docs/conf.py
@@ -223,7 +223,7 @@ html_theme_options = {
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
 #
-# html_title = 'uamx-social-auth v0.1.0'
+# html_title = 'uamx-social-auth v0.1.2'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #
--- platform_plugin_uamx_social_auth/__init__.py
+++ platform_plugin_uamx_social_auth/__init__.py
@@ -4,6 +4,6 @@ Social authentication pipeline for UAMx
 import os
 from pathlib import Path
 
-__version__ = '0.1.1.test'
+__version__ = '0.1.2'
 
 ROOT_DIRECTORY = Path(os.path.dirname(os.path.abspath(__file__)))
